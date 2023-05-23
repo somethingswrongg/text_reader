eba2056f820ad2a4b047c48a887162dd5ba836c6
name <vss0987@yandex.ru>
2023-05-23 19:22:09 +0300

    added static files

--- text_reader/views.py
+++ text_reader/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html", context={'body': '<h1>GdsfdhfhD</h1>'})
 
 
 def about(request):
